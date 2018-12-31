d3d0118788a966341ab2e60fe235da5003b187ca
Brendan O'Donoghue <bodonoghue85@gmail.com>
2018-12-31 14:29:25 +0000

    increase max norm from 10 to 1000

--- include/aa.h
+++ include/aa.h
@@ -45,7 +45,7 @@ aa_int aa_apply(aa_float *f, const aa_float *x, AaWork *a);
  */
 void aa_finish(AaWork *a);
 
-#define MAX_AA_NRM (10.0)
+#define MAX_AA_NRM (1000.0)
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
