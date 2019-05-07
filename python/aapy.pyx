5fb96fe5f07e7cf195e8a427ed86b00439e10258
Brendan O'Donoghue <bodonoghue85@gmail.com>
2019-05-07 16:58:17 -0500

    add python / cython interface

---
+++ python/aapy.pyx
@@ -0,0 +1,34 @@
+import numpy as np
+
+cdef extern from "../src/aa.c":
+    # C is include here so that it doesn't need to be compiled externally
+    pass
+
+cdef extern from "../include/aa.h":
+    ctypedef struct AaWork:
+        pass
+    AaWork *aa_init(int, int, int)
+    int aa_apply(double*, const double*, AaWork*)
+    void aa_finish(AaWork *)
+
+cdef class AndersonAccelerator(object):
+    cdef AaWork* _wrk
+    def __cinit__(self, dim, mem, type1):
+        self._wrk = aa_init(dim, mem, type1)
+
+    def apply(self, f, x):
+        if not f.flags['C_CONTIGUOUS']:
+            f = np.ascontiguousarray(f) # Makes a contiguous copy of the numpy array.
+        if not x.flags['C_CONTIGUOUS']:
+            x = np.ascontiguousarray(x) # Makes a contiguous copy of the numpy array.
+
+        cdef double[::1] f_memview = f
+        cdef double[::1] x_memview = x
+
+        success = aa_apply(&f_memview[0], &x_memview[0], self._wrk)
+
+        return success
+
+    def __dealloc__(self):
+        aa_finish(self._wrk)
+
