9904dfabbf8285923bfb3daa6f201219f4346622
Brendan O'Donoghue <bodonoghue85@gmail.com>
2019-05-07 17:43:20 -0500

    setup cleanup

--- python/setup.py
+++ python/setup.py
@@ -2,15 +2,16 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-examples_extension = Extension(
+aa_extension = Extension(
     name="aa",
     sources=["aapy.pyx"],
     library_dirs=["../src"],
     include_dirs=["../include"],
     libraries=['lapack', 'blas']
 )
+
 setup(
     name="aa",
-    ext_modules=cythonize([examples_extension])
+    ext_modules=cythonize([aa_extension])
 )
 
