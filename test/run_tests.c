b7987949c7c18d74f36c1a97e47a883d95aa3755
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-10-02 12:00:56 +0100

    adding tests

---
+++ test/run_tests.c
@@ -0,0 +1,171 @@
+/* Gradient descent (GD) on convex quadratic */
+#include "aa.h"
+#include "aa_blas.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "minunit.h"
+
+/* default parameters */
+#define SEED (1234)
+#define DIM (100)
+#define MEM (5)
+#define TYPE1_REGULARIZATION (1e-3)
+#define TYPE2_REGULARIZATION (0)
+#define SAFEGUARD_TOLERANCE (2.0)
+#define MAX_AA_NORM (1e10)
+#define ITERS (50000)
+#define STEPSIZE (0.01)
+#define PRINT_INTERVAL (500)
+#define VERBOSITY (1)
+
+int tests_run = 0;
+
+/* duplicate these with underscore prefix */
+typedef struct _timer {
+  struct timespec tic;
+  struct timespec toc;
+} _timer;
+
+void _tic(_timer *t) {
+  clock_gettime(CLOCK_MONOTONIC, &t->tic);
+}
+
+aa_float _tocq(_timer *t) {
+  struct timespec temp;
+
+  clock_gettime(CLOCK_MONOTONIC, &t->toc);
+
+  if ((t->toc.tv_nsec - t->tic.tv_nsec) < 0) {
+    temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec - 1;
+    temp.tv_nsec = 1e9 + t->toc.tv_nsec - t->tic.tv_nsec;
+  } else {
+    temp.tv_sec = t->toc.tv_sec - t->tic.tv_sec;
+    temp.tv_nsec = t->toc.tv_nsec - t->tic.tv_nsec;
+  }
+  return (aa_float)temp.tv_sec * 1e3 + (aa_float)temp.tv_nsec / 1e6;
+}
+
+/* uniform random number in [-1,1] */
+static aa_float rand_float(void) {
+  return 2 * (((aa_float)rand()) / RAND_MAX) - 1;
+}
+
+static const char *gd(aa_int type1, aa_float relaxation) {
+  aa_int n = DIM, iters = ITERS, memory = MEM, seed = SEED;
+  aa_int i, one = 1;
+  aa_int verbosity = VERBOSITY;
+  aa_float neg_step_size = -STEPSIZE;
+  aa_float safeguard_tolerance = SAFEGUARD_TOLERANCE;
+  aa_float max_aa_norm = MAX_AA_NORM;
+  aa_float err = 0;
+  aa_float regularization;
+  aa_float *x, *xprev, *Qhalf, *Q, zerof = 0.0, onef = 1.0;
+  _timer aa_timer;
+  aa_float aa_time = 0;
+  x = malloc(sizeof(aa_float) * n);
+  xprev = malloc(sizeof(aa_float) * n);
+  Qhalf = malloc(sizeof(aa_float) * n * n);
+  Q = malloc(sizeof(aa_float) * n * n);
+
+  srand(seed);
+
+  if (type1) {
+    regularization = TYPE1_REGULARIZATION;
+  } else {
+    regularization = TYPE2_REGULARIZATION;
+  }
+
+  /* generate random data */
+  for (i = 0; i < n; i++) {
+    x[i] = rand_float();
+  }
+  for (i = 0; i < n * n; i++) {
+    Qhalf[i] = rand_float();
+  }
+
+  BLAS(gemm)
+  ("Trans", "No", &n, &n, &n, &onef, Qhalf, &n, Qhalf, &n, &zerof, Q, &n);
+
+  /* add small amount regularization */
+  for (i = 0; i < n; i++) {
+    Q[i + i * n] += 1e-6;
+  }
+
+  AaWork *a = aa_init(n, memory, type1, regularization, relaxation,
+                      safeguard_tolerance, max_aa_norm, verbosity);
+  for (i = 0; i < iters; i++) {
+    if (i > 0) {
+      _tic(&aa_timer);
+      aa_apply(x, xprev, a);
+      aa_time += _tocq(&aa_timer);
+    }
+
+    memcpy(xprev, x, sizeof(aa_float) * n);
+    /* x = x - step_size * Q * xprev */
+    BLAS(gemv)
+    ("No", &n, &n, &neg_step_size, Q, &n, xprev, &one, &onef, x, &one);
+
+    _tic(&aa_timer);
+    aa_safeguard(x, xprev, a);
+    aa_time += _tocq(&aa_timer);
+
+    err = BLAS(nrm2)(&n, x, &one);
+    if (i % PRINT_INTERVAL == 0) {
+      printf("Iter: %i, Err %.4e\n", i, err);
+    }
+  }
+  printf("Iter: %i, Err %.4e\n", i, err);
+  printf("AA time: %.4f seconds\n", aa_time / 1e3);
+  aa_finish(a);
+  free(Q);
+  free(Qhalf);
+  free(x);
+  free(xprev);
+
+  mu_assert_less("Failed to produce small error", err, 1e-6);
+
+  return 0;
+}
+
+static const char *gd_type1_relax1(void) {
+  return gd(1, 1.0);
+}
+
+static const char *gd_type1_relax15(void) {
+  return gd(1, 1.5);
+}
+
+static const char *gd_type2_relax1(void) {
+  return gd(0, 1.0);
+}
+
+static const char *gd_type2_relax15(void) {
+  return gd(0, 1.5);
+}
+
+static const char *all_tests(void) {
+  printf("type 1, relaxation 1.0\n");
+  mu_run_test(gd_type1_relax1);
+  printf("type 1, relaxation 1.5\n");
+  mu_run_test(gd_type1_relax15);
+  printf("type 2, relaxation 1.0\n");
+  mu_run_test(gd_type2_relax1);
+  printf("type 2, relaxation 1.5\n");
+  mu_run_test(gd_type2_relax15);
+  return 0;
+}
+
+int main(void) {
+  const char *result = all_tests();
+  if (result != 0) {
+    printf("%s\n", result);
+  } else {
+    printf("ALL TESTS PASSED\n");
+  }
+  printf("Tests run: %d\n", tests_run);
+
+  return result != 0;
+}
